a8a4da1e27fc0ee5af85a196c6984a97b3b6dcf5
onderonur <onuronder.onr@gmail.com>
2021-04-12 01:02:05 +0300

    package updates

--- client/src/modules/socket-io/SocketIoContext.tsx
+++ client/src/modules/socket-io/SocketIoContext.tsx
@@ -1,22 +1,22 @@
 import React, { useContext, useEffect, useState } from 'react';
-import socketIoClient from 'socket.io-client';
+import { io as socketIo, Socket } from 'socket.io-client';
 import { getToken, API_URL } from 'modules/shared/SharedUtils';
 import { Maybe } from 'modules/shared/SharedTypes';
+import { ReservedOrUserListener } from 'socket.io-client/build/typed-events';
 
-const SocketIoContext = React.createContext<
-  SocketIOClient.Socket | null | undefined
->(null);
+const SocketIoContext = React.createContext<Maybe<Socket>>(null);
 
 type SocketIoProviderProps = React.PropsWithChildren<{}>;
 
 export function SocketIoProvider({ children }: SocketIoProviderProps) {
-  const [io, setIo] = useState<Maybe<SocketIOClient.Socket>>(null);
+  const [io, setIo] = useState<Maybe<Socket>>(null);
 
   useEffect(() => {
     function prepare() {
-      const socket = socketIoClient(API_URL, {
+      const token = getToken();
+      const socket = socketIo(API_URL, {
         path: '/socket-io',
-        query: { token: getToken() },
+        query: token ? { token } : undefined,
       });
       setIo(socket);
       return () => {
@@ -37,7 +37,11 @@ function useSocketIo() {
   return io;
 }
 
-export function useSocketListener(event: string, fn: Function) {
+export function useSocketListener(
+  event: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  fn: ReservedOrUserListener<any, any, any>,
+) {
   const io = useSocketIo();
 
   useEffect(() => {
