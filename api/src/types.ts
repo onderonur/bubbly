c4c957d78c90f353a05d86af17c9c8cd85476ede
onderonur <onuronder.onr@gmail.com>
2021-04-12 00:52:05 +0300

    package updates

--- api/src/types.ts
+++ api/src/types.ts
@@ -1,5 +1,3 @@
-import { SocketUser } from './SocketUser';
-
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
@@ -13,14 +11,6 @@ declare global {
 export type ID = string;
 
 export type Maybe<T> = T | null | undefined;
-
-declare global {
-  namespace SocketIO {
-    export class Socket {
-      user: SocketUser;
-    }
-  }
-}
 
 export interface JwtTokenPayload {
   id: ID;
